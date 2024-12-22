5e3b559913d131892d9688dcf7933b54aec183a3
Elfeshawy <seifeldainelsayedelfeshawy@gmail.com>
2024-12-22 17:42:37 +0200

    delete search

--- assets/scripts/nav.js
+++ assets/scripts/nav.js
@@ -1,20 +1,24 @@
-const searchicon1 =document.querySelector("#searchicon1");
-const srcicon1=document.querySelector("#srchicon1");
-const search1=document.querySelector("#searchinput1");
+// const searchicon1 =document.querySelector("#searchicon1");
+// const srcicon1=document.querySelector("#srchicon1");
+// const search1=document.querySelector("#searchinput1");
 
-searchicon1.addEventListener("click",function(){
-    search1.style.display="flex";
-    searchicon1.style.display="none";
-})
+// searchicon1.addEventListener("click",function(){
+//     search1.style.display="flex";
+//     searchicon1.style.display="none";
+// })
 
-const searchicon2 =document.querySelector("#searchicon2");
-const srcicon2=document.querySelector("#srchicon2");
-const search2=document.querySelector("#searchinput2");
+// search1.addEventListener("blur",function(){
+//     search1.style.display="none";
+// })
 
-searchicon2.addEventListener("click",function(){
-    search2.style.display="flex";
-    searchicon2.style.display="none";
-})
+// const searchicon2 =document.querySelector("#searchicon2");
+// const srcicon2=document.querySelector("#srchicon2");
+// const search2=document.querySelector("#searchinput2");
+
+// searchicon2.addEventListener("click",function(){
+//     search2.style.display="flex";
+//     searchicon2.style.display="none";
+// })
 
 const bar =document.querySelector(".fa-bars");
 const cross =document.querySelector("#hdcross");
